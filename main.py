3f2d6a2b4f7049f66a2929b1323e0d26e8315d2a
venkat <venkat.virat07@gmail.com>
2023-04-09 15:45:49 -0400

    created v1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # !pip install transformers
 import pandas as pd
+import numpy as np
 import torch
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
@@ -9,13 +10,18 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from preprocess_data import preprocess
 
 # Load data
-df = pd.read_csv("Shortened_Data.csv")
-df=df.head(100)
+df = pd.read_csv("Shortened_Data_5000.csv")
+df['Summary'].replace('', np.nan, inplace=True)
+df.dropna(subset=['Summary'], inplace=True)
+df=df.dropna()
+# df=df.head(2560)
 
 # Preprocess text
 for i in range(len(df)):
-  text = preprocess(df.iloc[i,1])
-
+#     print(i)
+#     if(df.iloc[i,1]==nil)
+    df.iloc[i,1] = preprocess(df.iloc[i,1])
+    
 # Split data into train/test sets
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
@@ -35,19 +41,22 @@ tokenizer = BertTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
 train_encodings = tokenizer(train_df["Summary"].tolist(), truncation=True, padding=True, max_length=512)
 test_encodings = tokenizer(test_df["Summary"].tolist(), truncation=True, padding=True, max_length=512)
 
-for i in range(10):
-  print(train_encodings["input_ids"])
+# for i in range(10):
+#   print(train_encodings["input_ids"])
 
 # Convert labels to tensors
 train_labels = torch.tensor(train_df["Classification"].tolist())
 test_labels = torch.tensor(test_df["Classification"].tolist())
 
+batch_size = 16
+epochs = 1
+
 # Create PyTorch DataLoader objects
 train_dataset = torch.utils.data.TensorDataset(torch.tensor(train_encodings["input_ids"]), torch.tensor(train_encodings["attention_mask"]), torch.tensor(train_labels))
 test_dataset = torch.utils.data.TensorDataset(torch.tensor(test_encodings["input_ids"]), torch.tensor(test_encodings["attention_mask"]), torch.tensor(test_labels))
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=False)
-
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size, shuffle=False)
+# print("length(Train Loader):", len(train_loader))
 # Load BERT model
 
 # Train BERT model
@@ -55,9 +64,9 @@ optimizer = AdamW(model.parameters(), lr=2e-5, eps=1e-8)
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=len(train_loader) * 5)
 model.train()
 i=1
-for epoch in range(1):
+for epoch in range(epochs):
     for batch in train_loader:
-        print("batch: ",i,"\n")
+        print("epoch:",epoch+1,"/",epochs," batch:",i,"/",len(train_loader))
         i=i+1
         optimizer.zero_grad()
         input_ids, attention_mask, labels = batch
@@ -72,15 +81,42 @@ model.eval()
 with torch.no_grad():
     y_true = []
     y_pred = []
+    # print("len(batch):",len(batch))
+#     print("test_loader",test_loader)
     for batch in test_loader:
         input_ids, attention_mask, labels = batch
         outputs = model(input_ids, attention_mask=attention_mask)
         logits = outputs.logits
+        print("logits:",logits)
         predictions = torch.argmax(logits, axis=1)
+        print("predictions:",predictions)
+        print("len(y_true) ",len(y_true)," ")
         y_true += labels.tolist()
+        print("len(y_pred) ",len(y_pred))
         y_pred += predictions.tolist()
 
-print("Accuracy:", accuracy_score(y_true, y_pred))
-print("Precision:", precision_score(y_true, y_pred,average='micro'))
-print("Recall:", recall_score(y_true, y_pred,average='micro'))
-print("F1 score:", f1_score(y_true, y_pred,average='micro'))
+tp=0
+tn=0
+fp=0
+fn=0
+
+for i in range(len(y_true)):
+        if(y_true[i]==y_pred[i]):
+            if(y_true[i]==1):
+                tp=tp+1
+            else:
+                tn=tn+1
+        else:
+            if(y_true[i]==1):
+                fn=fn+1
+            else:
+                fp=fp+1
+Accuracy = (tp+tn)/(tp+tn+fp+fn)
+Precision = tp/(tp+fp)
+Recall = tp/(tp+fn)
+F1_Score = (2*Recall*Precision)/(Precision + Recall)
+
+print("Accuracy:", Accuracy)
+print("Precision:", Precision)
+print("Recall:", Recall)
+print("F1 score:", F1_Score)
